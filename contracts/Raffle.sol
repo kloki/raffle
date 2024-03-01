2efcd777d85b81572b96f4b74b39b1097e1f55b4
Koen Klinkers <k.klinkers@gmail.com>
2024-03-01 11:29:21 +0100

    add test

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -4,9 +4,18 @@ pragma solidity ^0.8.0;
 
 contract Raffle {
     address public owner;
+    address[] public participants;
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function join() public {
+        require(msg.sender != owner, "Cannot join your own raffle");
+        for (uint i = 0; i < participants.length; i++) {
+            require(msg.sender != participants[i], "Cannot join raffle twice");
+        }
+        participants.push(msg.sender);
     }
 
 }
--- test/Raffle.js
+++ test/Raffle.js
@@ -1,11 +1,35 @@
 const { expect } = require("chai");
 
 describe("Raffle contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
+  it("Deployment should set the correct owner", async function () {
     const [owner] = await ethers.getSigners();
 
     const raffle = await ethers.deployContract("Raffle");
 
-    expect(await raffle.owner).to.equal(owner);
+    expect(await raffle.owner()).to.equal(owner);
+  });
+  it("Cannot join own contract", async function () {
+    const raffle = await ethers.deployContract("Raffle");
+
+    await expect(raffle.join()).to.be.revertedWith(
+      "Cannot join your own raffle"
+    );
+    // expect(await raffle.participants().length).to.equal(0);
+  });
+  it("Success", async function () {
+    const [_, p1, p2] = await ethers.getSigners();
+    const raffle = await ethers.deployContract("Raffle");
+    await raffle.connect(p1).join();
+    await raffle.connect(p2).join();
+    // expect(await raffle.participants().length).to.equal(2);
+  });
+  it("Cannot join twice", async function () {
+    const [_, p1] = await ethers.getSigners();
+    const raffle = await ethers.deployContract("Raffle");
+    await raffle.connect(p1).join();
+    await expect(raffle.connect(p1).join()).to.be.revertedWith(
+      "Cannot join raffle twice"
+    );
+    // expect(await raffle.participants().length).to.equal(2);
   });
 });
